d91bdbc7899ef1cc397dc61e22a748dc61a0fa1c
Alexej Disterhoft <alexej@disterhoft.de>
2024-11-28 16:13:30 +0100

    feat: update provider module path and implement SopsFile function

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -67,20 +67,16 @@ func (p *ScaffoldingProvider) Configure(ctx context.Context, req provider.Config
 }
 
 func (p *ScaffoldingProvider) Resources(ctx context.Context) []func() resource.Resource {
-	return []func() resource.Resource{
-		NewExampleResource,
-	}
+	return []func() resource.Resource{}
 }
 
 func (p *ScaffoldingProvider) DataSources(ctx context.Context) []func() datasource.DataSource {
-	return []func() datasource.DataSource{
-		NewExampleDataSource,
-	}
+	return []func() datasource.DataSource{}
 }
 
 func (p *ScaffoldingProvider) Functions(ctx context.Context) []func() function.Function {
 	return []func() function.Function{
-		NewExampleFunction,
+		NewSopsFileFunction,
 	}
 }
 
