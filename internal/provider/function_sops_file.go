c3a3c7cd2ef0608d8eff09d41d445ae184ac912b
Alexej Disterhoft <alexej@disterhoft.de>
2024-11-28 16:13:32 +0100

    feat: add format parameter to SopsFile function and implement dynamic type conversion

--- internal/provider/function_sops_file.go
+++ internal/provider/function_sops_file.go
@@ -2,16 +2,19 @@ package provider
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 
 	"github.com/getsops/sops/v3/decrypt"
 	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/function"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/nobbs/terraform-provider-sops/internal/typeutils"
 )
 
 var sopsFileReturnAttrTypes = map[string]attr.Type{
-	"raw": types.StringType,
+	"raw":  types.StringType,
+	"data": types.DynamicType,
 }
 
 var _ function.Function = &SopsFile{}
@@ -36,6 +39,11 @@ func (f *SopsFile) Definition(ctx context.Context, req function.DefinitionReques
 				Name:        "file",
 				Description: "The path to the sops encrypted file to read and decrypt.",
 			},
+			function.StringParameter{
+				Name:           "format",
+				Description:    "The format of the file to read. If not provided, the format will be inferred from the file extension.",
+				AllowNullValue: true,
+			},
 		},
 
 		Return: function.ObjectReturn{
@@ -45,24 +53,44 @@ func (f *SopsFile) Definition(ctx context.Context, req function.DefinitionReques
 }
 
 func (f *SopsFile) Run(ctx context.Context, req function.RunRequest, resp *function.RunResponse) {
-	var file string
+	var file, format string
 
-	resp.Error = req.Arguments.Get(ctx, &file)
+	resp.Error = req.Arguments.Get(ctx, &file, &format)
 	if resp.Error != nil {
 		return
 	}
 
 	// decrypt
-	cleartext, err := decrypt.File(file, "binary")
+	cleartext, err := decrypt.File(file, format)
 	if err != nil {
 		resp.Error = function.NewFuncError(fmt.Sprintf("failed to decrypt file: %v", err))
+		return
+	}
+
+	var data any
+	err = json.Unmarshal(cleartext, &data)
+	if err != nil {
+		resp.Error = function.NewFuncError(fmt.Sprintf("failed to unmarshal decrypted data: %v", err))
+		return
+	}
+
+	jsonBytes, err := json.Marshal(data)
+	if err != nil {
+		resp.Error = function.NewFuncError(fmt.Sprintf("failed to marshal decrypted data: %v", err))
+		return
+	}
+
+	dt, err := typeutils.JSONToDynamicImplied(jsonBytes)
+	if err != nil {
+		resp.Error = function.NewFuncError(fmt.Sprintf("failed to convert decrypted data to dynamic: %v", err))
 		return
 	}
 
 	result, diags := types.ObjectValue(
 		sopsFileReturnAttrTypes,
 		map[string]attr.Value{
-			"raw": types.StringValue(string(cleartext)),
+			"raw":  types.StringValue(string(cleartext)),
+			"data": dt,
 		},
 	)
 
