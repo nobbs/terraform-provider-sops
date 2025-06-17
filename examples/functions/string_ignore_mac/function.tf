4c66b1f26c932ec378d05541b3e2b4f4425316ee
Alexej Disterhoft <alexej@disterhoft.de>
2025-06-17 22:04:52 +0200

    docs: add examples for file_ignore_mac and string_ignore_mac functions

---
+++ examples/functions/string_ignore_mac/function.tf
@@ -0,0 +1,24 @@
+data "http" "basic-yaml-mac-mismatch" {
+  url = "https://raw.githubusercontent.com/nobbs/terraform-provider-sops/refs/heads/main/test/fixtures/basic-mac-mismatch.sops.yaml"
+}
+
+output "basic-yaml-mac-mismatch" {
+  value = provider::sops::string(data.http.basic-yaml-mac-mismatch.response_body)
+}
+
+# basic-yaml-mac-mismatch = {
+#   "data" = {
+#     "abc" = "xyz"
+#     "floats" = 0.000000000314
+#     "integers" = 123
+#     "truthy" = true
+#   }
+#   "raw" = <<-EOT
+#   {
+#   	"abc": "xyz",
+#   	"integers": 123,
+#   	"truthy": true,
+#   	"floats": 3.14e-10
+#   }
+#   EOT
+# }
